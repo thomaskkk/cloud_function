0d08d9d605ec8558ba469f0ad7f89899e81f58ea
Thomas Kuryura <thomas.adm@gmail.com>
2023-09-03 11:31:55 -0300

    [IS-6] Simple test

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,27 +3,29 @@ import json
 
 @pytest.fixture(scope="module")
 def input_json():
-    return json.dumps(
-        {
-            "Account_number": 12345,
-            "Report_number": 1234567,
-            "Report_token": "largest_token_ever_123",
-            "Cycletime": {
-                "Percentiles": [
-                    50,
-                    85,
-                    95
-                ]
-            },
-            "Throughput_range": 90,
-            "Montecarlo": {
-                "Simulations": 10000,
-                "Simulation_days": 14,
-                "Percentiles": [
-                    50,
-                    85,
-                    95
-                ]
-            }
-        }
-    )
+    input_json = '''
+{
+    "Account_number": 12345,
+    "Report_number": 1234567,
+    "Report_token": "largest_token_ever_123",
+    "Cycletime": {
+        "Percentiles": [
+            50,
+            85,
+            95
+        ]
+    },
+    "Throughput_range": 90,
+    "Montecarlo": {
+        "Simulations": 10000,
+        "Simulation_days": 14,
+        "Percentiles": [
+            50,
+            85,
+            95
+        ]
+    }
+}
+'''
+    return json.loads(input_json)
+
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,22 +1,11 @@
 import src.main as main
-import json
-
+import pytest
 
 def test_config_is_generated_from_json_request_body(input_json: str):
     # Given
-    dict_json = json.loads(input_json)
-
     # When
     error = main.test_config(input_json)
 
     # Then
     assert error is None
 
-def test_url_generates_from_config_json():
-    # Given
-
-
-    # When
-    
-    # Then
-    assert True
