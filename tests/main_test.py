12a35068cd2709e5ba90369646fa84b64f976907
Thomas Kuryura <thomas.adm@gmail.com>
2023-09-03 22:00:52 -0300

    [IS-4] Tests for mc simulation

--- tests/main_test.py
+++ tests/main_test.py
@@ -56,3 +56,31 @@ def test_valid_dataframe_with_date_and_issue_columns():
         'issues': [2, 1, 1]
     })
     pd.testing.assert_frame_equal(throughput, expected_throughput)
+
+def test_mc_simulation():
+    # Given
+    throughput = pd.DataFrame({
+        "date": [
+            "2021-01-01", "2021-01-02", "2021-01-03", "2021-01-04", "2021-01-05",
+            "2021-01-06", "2021-01-07", "2021-01-08", "2021-01-09", "2021-01-10",
+            "2021-01-11", "2021-01-12", "2021-01-13", "2021-01-14"],
+        "issues": [2, 1, 1, 2, 1, 0,  0, 1, 0, 0, 1, 0, 0, 4]
+    })
+    cfg = {
+        "Montecarlo": {
+            "Simulations": 10000,
+            "Simulation_days": 14,
+            "Percentiles": [50, 85, 95]
+        }
+    }
+
+    # When
+    result = main.run_simulation(cfg, throughput)
+
+    # Then
+    expected_result = pd.DataFrame({
+        'montecarlo 50%': ['13'],
+        'montecarlo 85%': ['9'],
+        'montecarlo 95%': ['7']
+    }, index=['issues'], dtype='int64')
+    pd.testing.assert_frame_equal(result, expected_result)
